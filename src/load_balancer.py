c571efe1dc25ec9f630ccb70f294ca0ec86f3d03
Pranshu Gupta <pranshug258@outlook.com>
2025-08-07 00:12:13 -0700

    extracted an abstract class for load balancer

--- src/load_balancer.py
+++ src/load_balancer.py
@@ -1,29 +1,19 @@
-import itertools
-from typing import Set, Optional
+from typing import Set, Optional, List
+from abc import ABC, abstractmethod
 
-class LoadBalancer:
-    def __init__(self):
-        self.registered_backends: Set[str] = set()
-        self.backend_iter: Optional[itertools.cycle] = None
-
-    def update_backend_iter(self):
-        if self.registered_backends:
-            self.backend_iter = itertools.cycle(list(self.registered_backends))
-        else:
-            self.backend_iter = None
-
+class LoadBalancer(ABC):
+    @abstractmethod
     def register(self, url: str):
-        self.registered_backends.add(url)
-        self.update_backend_iter()
+        pass
 
+    @abstractmethod
     def unregister(self, url: str):
-        self.registered_backends.discard(url)
-        self.update_backend_iter()
+        pass
 
+    @abstractmethod
     def get_next_backend(self) -> Optional[str]:
-        if not self.backend_iter:
-            return None
-        return next(self.backend_iter)
+        pass
 
-    def list_backends(self):
-        return list(self.registered_backends)
+    @abstractmethod
+    def list_backends(self) -> List[str]:
+        pass
---
+++ src/round_robin_load_balancer.py
@@ -0,0 +1,31 @@
+import itertools
+from typing import Set, Optional, List
+
+from src.load_balancer import LoadBalancer
+
+class RoundRobinLoadBalancer(LoadBalancer):
+    def __init__(self):
+        self.registered_backends: Set[str] = set()
+        self.backend_iter: Optional[itertools.cycle] = None
+
+    def update_backend_iter(self):
+        if self.registered_backends:
+            self.backend_iter = itertools.cycle(list(self.registered_backends))
+        else:
+            self.backend_iter = None
+
+    def register(self, url: str):
+        self.registered_backends.add(url)
+        self.update_backend_iter()
+
+    def unregister(self, url: str):
+        self.registered_backends.discard(url)
+        self.update_backend_iter()
+
+    def get_next_backend(self) -> Optional[str]:
+        if not self.backend_iter:
+            return None
+        return next(self.backend_iter)
+
+    def list_backends(self) -> List[str]:
+        return list(self.registered_backends)
