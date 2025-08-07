f4afc55e2efa617b147faecabda7e8c92aa7bec7
Pranshu Gupta <pranshug258@outlook.com>
2025-08-07 02:08:18 -0700

    reordered

--- scripts/start_backends.sh
+++ scripts/start_backends.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # Script to start multiple backend servers on different ports
 
+# Start the proxy server
+echo "Starting proxy server on port 8000"
+nohup uvicorn src.proxy:app --port 8000 > logs/backend_8000.log 2>&1 &
+
 # Number of backend servers to start (default: 2)
 NUM_SERVERS=${1:-2}
 # Proxy URL (default: http://localhost:8000)
 PROXY_URL=${2:-http://localhost:8000}
 # Starting port (default: 8001)
 START_PORT=${3:-8001}
-
 
 for ((i=0; i<$NUM_SERVERS; i++)); do
   PORT=$((START_PORT + i))
@@ -16,6 +19,3 @@ for ((i=0; i<$NUM_SERVERS; i++)); do
 done
 
 echo "Started $NUM_SERVERS backend servers. Logs: backend_<PORT>.log"
-# Start the proxy server
-echo "Starting proxy server on port 8000"
-nohup uvicorn src.proxy:app --port 8000 > logs/backend_8000.log 2>&1 &
