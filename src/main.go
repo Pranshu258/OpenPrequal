61f3710a94927a6a9d540b3223eb1e655a35eb5f
Pranshu Gupta <pranshug258@outlook.com>
2025-08-06 23:37:42 -0700

    updated print statement

--- src/main.go
+++ src/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	log.Println("Starting OpenPrequal sidecar proxy...")
+	log.Println("Starting OpenPrequal Proxy...")
 
 	// Load configuration
 	config := LoadConfig()
