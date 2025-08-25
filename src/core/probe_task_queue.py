8ce1e6bfa2d7d61f390e2206404a9bb1a236ee51
Pranshu Gupta <pranshug258@outlook.com>
2025-08-24 17:33:51 -0700

    removed unnecesaary locking

--- src/core/probe_task_queue.py
+++ src/core/probe_task_queue.py
@@ -16,30 +16,23 @@ class ProbeTaskQueue:
     @Profiler.profile
     def __init__(self):
         self._set = set()
-        self._lock = asyncio.Lock()
-        self._not_empty = asyncio.Condition()
+        self._queue = asyncio.Queue()
 
     @Profiler.profile
     async def add_task(self, backend_id):
-        async with self._lock:
-            if backend_id not in self._set:
-                self._set.add(backend_id)
-                async with self._not_empty:
-                    self._not_empty.notify()
-                logger.debug(f"Added probe task for backend {backend_id}")
-            else:
-                logger.debug(f"Probe task for backend {backend_id} already in set")
+        if backend_id not in self._set:
+            self._set.add(backend_id)
+            await self._queue.put(backend_id)
+            logger.debug(f"Added probe task for backend {backend_id}")
+        else:
+            logger.debug(f"Probe task for backend {backend_id} already in set")
 
     @Profiler.profile
     async def get_task(self):
-        while True:
-            async with self._not_empty:
-                await self._not_empty.wait_for(lambda: len(self._set) > 0)
-            async with self._lock:
-                if self._set:
-                    backend_id = self._set.pop()
-                    logger.debug(f"Got probe task for backend {backend_id}")
-                    return backend_id
+        backend_id = await self._queue.get()
+        self._set.discard(backend_id)
+        logger.debug(f"Got probe task for backend {backend_id}")
+        return backend_id
 
     @Profiler.profile
     def task_done(self):
